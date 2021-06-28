552f79710bdf2694abaca3288d38564e5f165546
Anchobies <anthonychung97@gmail.com>
2021-06-28 17:44:53 -0400

    commit tree b16b4707fe26fd68c11affe4853d8a1a395034cc

--- src/components/MovieShow.js
+++ src/components/MovieShow.js
@@ -5,15 +5,19 @@ import MovieShow from "./MovieShow";
 
 function MoviesPage({ movies }) {
   const match = useRouteMatch();
-  console.log(match);
 
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+
       <Route path={`${match.url}/:movieId`}>
         <MovieShow movies={movies} />
       </Route>
     </div>
   );
 }
-export default MoviesPage;
+
+export default MoviesPage; 
